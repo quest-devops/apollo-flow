4a8021218cfdd7470e010c0684b209054729f3e9
quest-devops <leonardo@apollosolution.com.br>
2026-07-06 17:15:04 -0300

    brand: logo oficial Apollo vetorizado dos arquivos originais (potrace) + assets binarios reais

--- packages/propel/src/icons/brand/plane-wordmark.tsx
+++ packages/propel/src/icons/brand/plane-wordmark.tsx
@@ -6,29 +6,9 @@
 
 import * as React from "react";
 
+import { PlaneLockup } from "./plane-lockup";
 import type { ISvgIcons } from "../type";
 
-export function PlaneWordmark({ width = "146", height = "44", className, color = "currentColor" }: ISvgIcons) {
-  return (
-    <svg
-      width={width}
-      height={height}
-      viewBox="0 0 146 44"
-      fill={color}
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-    >
-      <text
-        x="2"
-        y="31"
-        fontFamily='ui-sans-serif, system-ui, -apple-system, "Segoe UI", sans-serif'
-        fontSize="28"
-        fontWeight="700"
-        letterSpacing="2.5"
-        fill={color}
-      >
-        APOLLO
-      </text>
-    </svg>
-  );
+export function PlaneWordmark(props: ISvgIcons) {
+  return <PlaneLockup {...props} />;
 }
